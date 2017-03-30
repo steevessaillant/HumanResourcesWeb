39e4b55b0e2885b78b5227a1cd3cf1e7e79c6ce6
X122687 <X122687@FR09542731W>
2017-03-30 15:00:11 +0200

    Add project files.

---
+++ HumanResourcesWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HumanResourcesWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ HumanResourcesWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HumanResourcesWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
